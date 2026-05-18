f81d896177079fb6fc5bf040ca192d8da6bc938a
Martín Godoy <martin.godoy.mg@estudiante.uc.cl>
2026-05-18 16:13:25 -0400

    generador commit

--- data/generador_mapa.py
+++ data/generador_mapa.py
@@ -1,2 +1,4 @@
 #Esto solo por el momento, despues lo hago realista
 
+with open('mapa_test.txt', 'w') as archivo:
+    print('nodo_id, nodo_fila, nodo_columna, tipo_de_terreno, foco_inicial, población, viviendas')
